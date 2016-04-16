a28801339e99726cac2c0d22bdc5efe457d24387
Fan Zhang <zhafang@google.com>
2016-04-15 17:23:21 -0700

    Added keyframe

--- mapdata/include/keyframe.hpp
+++ mapdata/include/keyframe.hpp
@@ -1,13 +1,11 @@
 #include <opencv2/opencv.hpp>
 
-#include "feature_set.hpp"
-
 namespace vio {
 
-class Frame {
+class KeyFrame {
  public:
   // TODO: Really need to copy the image?
-  explicit Frame(const cv::Mat &image) { image.copyTo(image_); }
+  explicit KeyFrame(const cv::Mat &image) { image.copyTo(image_); }
 
   const cv::Mat &GetImage() const { return image_; }
 
@@ -16,11 +14,20 @@ class Frame {
     features.descriptors.copyTo(features_.descriptors);
   }
 
-  const FeatureSet &GetFeatures() const { return features_; }
+  void set_keypoints(std::vector<cv::KeyPoint> &keypoints) {
+    keypoints_ = std::move(keypoints);
+  }
+  void set_descriptors(cv::Mat &descriptors) {
+    descriptors_ = descriptors;
+  }
+
 
  private:
   cv::Mat image_;
-  FeatureSet features_;
+
+  std::vector<cv::KeyPoint> keypoints_;
+  cv::Mat descriptors_;
+
 };
 
 }  // namespace vio
