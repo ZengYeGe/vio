b4906711d7de0c89829d7c488a3afbde8da24ac6
Fan Zhang <fz.zhangfan@gmail.com>
2016-05-03 00:38:25 -0700

    Added test video app

--- test_apps/src/test_video.cpp
+++ test_apps/src/test_video.cpp
@@ -1,6 +1,39 @@
 #include "vio_app.hpp"
 
+#include <opencv2/videoio/videoio.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
 int TestVideo(Options option) {
+
+  cv::VideoCapture capture(option.video_filename); 
+  if (!capture.isOpened()) {
+      std::cerr << "Failed to open the video device, video file or image sequence!\n" << std::endl;
+      return 1;
+  }
+
+  int n = 0;
+  string window_name = "video | q or esc to quit";
+  std::cout << "press space to save a picture. q or esc to quit" << std::endl;
+  cv::namedWindow(window_name, WINDOW_KEEPRATIO); //resizable window;
+  cv::Mat frame;
+
+  for (;;) {
+    capture >> frame;
+    if (frame.empty())
+      break;
+
+    cv::imshow(window_name, frame);
+    char key = (char)waitKey(30);
+
+    switch (key) {
+      case 'q':
+      case 'Q':
+      case 27: //escape key
+        return 0;
+      default:
+        break;
+    }
+  }
 
   return 0;
 }
